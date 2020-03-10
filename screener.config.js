c326766ee42fba4440ee19b6a858c94e18c73a59
Andy Wyatt <andy@saucelabs.com>
2020-03-10 10:46:20 +0000

    initial commit

--- screener.config.js
+++ screener.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  projectRepo: 'saucelabs/screener-components',
+  projectRepo: 'tubasauce/screener-components',
   storybookConfigDir: '.storybook',
   storybookStaticDir: 'public',
-  apiKey: process.env.SCREENER_API_KEY,
+  apiKey: '1ee83434-cf8b-43e5-bc9a-4f879d6609f5',
   resolution: '1024x768',
   baseBranch: 'master'
 };
