00876a23af46ecb1494834ab3606c9f2ae282136
Andy Wyatt <andy@saucelabs.com>
2020-03-10 13:57:15 +0000

    updated screener config to use env var

--- screener.config.js
+++ screener.config.js
@@ -2,7 +2,7 @@ module.exports = {
   projectRepo: 'tubasauce/visualDemo',
   storybookConfigDir: '.storybook',
   storybookStaticDir: 'public',
-  apiKey: '1ee83434-cf8b-43e5-bc9a-4f879d6609f5',
+  apiKey: process.env.SCREENER_API_KEY,
   resolution: '1024x768',
   baseBranch: 'master'
 };
